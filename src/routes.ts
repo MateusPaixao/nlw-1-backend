9e939454aa8e1421be6dd63eddc0997a22d33e19
Mateus Paixão <mateus.paixao@corebiz.com.br>
2020-06-04 10:26:51 -0300

    add seeds, and routes for items

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
-import express, { response } from 'express'
+import express from 'express'
+import knex from './database/connection'
 
 const routes = express.Router()
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Hello' })
+routes.get('/items', async (request, response) => {
+
+    const items = await knex('items').select('*')
+
+    return response.json(items)
 })
 
 export default routes
